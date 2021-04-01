1c846b4c4e2d5c2c6a068a234345ea7b3aa90cd2
taymoorkhan <taymoorkhan@hotmail.com>
2021-04-01 16:13:12 -0700

    idek
    idek

--- main.py
+++ main.py
@@ -45,23 +45,43 @@ def panda_open_file2(list_FILENAME):
 
 
 def google_geo_finder(FILENAMES = "idk"):
+  
+  
+  
     gmaps = googlemaps.Client(key = "AIzaSyAZIWy7l39FrbssxdhBnuDXr9OtAhm5NV0")
-    df = pd.read_csv("LA_Traffic_Volume.csv")
-    all_coordinates = []
-    for location in df["Location"]:
-        try:
-            geocode_result = gmaps.geocode(location)
-            # print(geocode_result)
-            result_we_want = geocode_result.pop()
-            coordinates = (str(result_we_want["geometry"]["location"]))[1:-1]
-            coordinates = coordinates.replace("'lat': ", "")
-            coordinates = coordinates.replace("'lng': ", "")
-            all_coordinates.append(coordinates)
-            print(coordinates)
-        except:
-            all_coordinates.append("N/A")
-    df["coordinates"] = all_coordinates
-    df.to_csv("LA_Traffic_Volume_With_Coordinates2.csv")
+    df = pd.read_csv("NY_Traffic_Vol_2014-2019.csv")
+    for location in str(df["Roadway Name"] + "from" + df["From"] + "to" + df["To"]):
+        geocode_result = gmaps.geocode(location)
+        result_we_want = geocode_result.pop()
+        result_we_want = result_we_want["address_components"]
+        result_we_want = result_we_want[1]
+        result_we_want = result_we_want["long_name"]
+        print(result_we_want)
+    # for location in str(df["Roadway Name"] + "from" + df["From"] + "to" + df["To"]):
+    #     geocode_result = gmaps.geocode(location)
+    #     result_we_want = geocode_result.pop()
+        
+    # print(geocode_result)
+    print("wtf")
+    # all_coordinates = []
+    # for location in df["Location"]:
+  
+  
+  
+    """For checking geocode in LA"""
+    #     try:
+    #         geocode_result = gmaps.geocode(location)
+    #         # print(geocode_result)
+    #         result_we_want = geocode_result.pop()
+    #         coordinates = (str(result_we_want["geometry"]["location"]))[1:-1]
+    #         coordinates = coordinates.replace("'lat': ", "")
+    #         coordinates = coordinates.replace("'lng': ", "")
+    #         all_coordinates.append(coordinates)
+    #         print(coordinates)
+    #     except:
+    #         all_coordinates.append("N/A")
+    # df["coordinates"] = all_coordinates
+    # df.to_csv("LA_Traffic_Volume_With_Coordinates2.csv")
     
  
     # reverse_geocode_result = gmaps.reverse_geocode((40.714224, -73.961452))
@@ -74,15 +94,30 @@ def google_geo_finder(FILENAMES = "idk"):
     #                                  departure_time=now)
 
 
-def main():
-    name_input = "idk"
-    FILENAMES = []
-    while name_input != "":
-        name_input = input("Enter file name: ")
-        if name_input != "":
-            FILENAMES.append(name_input)
-    
-    google_geo_finder()
+def play():
+    df = pd.read_csv("NY_Traffic_Vol_2014-2019.csv")
+    # totals = []
+    # print(df)
+    # print(df["0"])
+    # print("wtf")
+    sum_column = str(df["Roadway Name"]) + "from" + str(df["From"]) + "to" + str(df["To"])
+    # print(sum_column[1])
+    df["full"] = sum_column
+    print(sum_column)
+    # print(df["full"][1])
+    # print(df["Roadway Name"])
+    # print(df)
+    # df["full address"] = sum_column
+    # df.to_csv("NY_Traffic_Vol_2014-2019(3).csv")
+    # def main():
+    #     name_input = "idk"
+    #     FILENAMES = []
+    #     while name_input != "":
+    #         name_input = input("Enter file name: ")
+    #         if name_input != "":
+    #             FILENAMES.append(name_input)
+        
+    #     google_geo_finder()
     # coordinates = geo_finder(FILENAMES)
     # value = panda_open_file2(FILENAMES)
 
@@ -92,4 +127,7 @@ def main():
 
 
 
-main()
+# main()
+
+play()
+# google_geo_finder()
